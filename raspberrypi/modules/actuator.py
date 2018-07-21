4f412dbaa23e749ebee1247a08f1eec854c7c55a
jonahau <39183678+jonahau@users.noreply.github.com>
2018-07-21 15:46:09 -0700

    Added on_subscribe function

--- raspberrypi/modules/actuator.py
+++ raspberrypi/modules/actuator.py
@@ -1,14 +1,19 @@
+#process network traffic import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
 
-INTERLOCK_PIN = 20
-LATCH_PIN = 21
+MOUNT_PIN = 20
+DOOR_PIN = 21
 
-GPIO.setup(INTERLOCK_PIN, GPIO.OUT)
-GPIO.setup(LATCH_PIN, GPIO.OUT)
+GPIO.setup(MOUNT_PIN, GPIO.OUT)
+GPIO.setup(DOOR_PIN, GPIO.OUT)
+
+MOUNT = GPIO.PWM(MOUNT_PIN,50)
+DOOR = GPIO.PWM(DOOR_PIN, 50)
 
 # MQTT Client demo
 # Continuously monitor two different MQTT topics for data,
@@ -20,19 +25,42 @@ def on_connect(client, userdata, flags, rc):
  
     # Subscribing in on_connect() - if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("ASSDRONE/Sensor/Alarm")
- 
+    client.subscribe('motion')
+    client.subscribe('dropbox')
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    if msg.payload == "Motion Detected":
+    if msg.payload == 'Motion Detected':
         print("Motion Detected: Disbursing Drone")
-        #INTERLOCK
+        #INTERLOCK DOOR UNLOCKS
+        p.start(11.5)
+        time.sleep(1)
+        p.stop()
+        GPIO.cleanup()
+        
         #TURN DRONE ON
-        #WAIT 10
+        publish.single("drone", "Start Drone", hostname="test.mosquitto.org")
+        #WAIT 4
+        time.sleep(4)
+        
         #LATCH
-    else 
+        p.start(11.5)
+        time.sleep(1)
+        p.stop()
+        
+    elif ((msg.payload == 'Reset Mount') & (msg.topic == 'dropbox')):
         print("Message Received" + msg.topic+" "+str(msg.payload))
+        MOUNT.start(7)
+        time.sleep(1)
+        MOUNT.stop()
+  
+    elif ((msg.payload == 'Reset Door') & (msg.topic == 'dropbox')):
+        DOOR.start(7)
+        time.sleep(1)
+        DOOR.stop()
+
+def on_subscribe(client, userdata, flags, rc):
+    print("Subscribed to motion and dropbox topics.")
 
 # Create an MQTT client and attach our routines to it.
 client = mqtt.Client()
@@ -45,7 +73,4 @@ client.connect("test.mosquitto.org", 1883, 60)
 # Process network traffic and dispatch callbacks. This will also handle
 # reconnecting. Check the documentation at
 # https://github.com/eclipse/paho.mqtt.python
-# for information on how to use other loop*() functions
 client.loop_forever()
-
-
